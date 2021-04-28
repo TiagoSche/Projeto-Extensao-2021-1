0f09eb9c2361750a669eab0d1a6d39dc23abd6de
Tiago Schneider <tiagoschneider1@gmail.com>
2021-04-28 19:56:04 -0300

    consertando erros de cadastro e login da ong

--- PHPPaginas/CadastroOng.php
+++ PHPPaginas/CadastroOng.php
@@ -1,7 +1,4 @@
-<?php
-    session_start();
-   
-?>
+
 <!DOCTYPE html>
 <html lang="pt_br">
 <head>
@@ -16,36 +13,8 @@
     <title>Login ONG's</title>
 </head>
 <body>
-        <nav class="navbar navbar-expand-lg " id="mainNav">
-            <div class="container">
-                <nav class="navbar navbar-dark">
-                <div class="dropdown">
-                    <button class="navbar-toggler bg-warning"  type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="dropdown-content">
-                    <ul class="navbar-nav text-uppercase ml-auto">
-                        <li class="nav-item"><a class="nav-link" href="#">HISTÓRIA</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">ONGs</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">DOE</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">CONTATO</a></li>
-                    </ul>
-                    </div>    
-                </div>
-                </nav>
-                <a href="../PHPPaginas/index.php"><img src="../IMG/logotipo.png" style="width: 200px;" alt=""></a>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav text-uppercase ml-auto">
-                        <li class="nav-item"><a class="nav-link" href="#">HISTÓRIA</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">| ONGs</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">| DOE</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">| CONTATO |</a></li>
-                        <li class="nav-item"> <a class="nav-link btn btn-warning" href="../PHPPaginas/loginambos.php">LOGIN</a></li>
-                    </ul>
-                </div>
-                
-            </div>
-            </nav>
+    <?php   include "header.php"; ?>
+
         <!--------------------------------------------Login ONG----------------------------------------------->
             <!----<form method="$_POST" action="../PHPConsultas/loginOng-action.php" style="margin: 10% 0% 0% 20%" class="form_login">
                 <h2>Login ONG</h2></br>
